3e402fe40f8860751f26c28b794a0ac9b9d73db1
Tim Spencer <tspencer@cloudpassage.com>
2015-11-23 09:36:39 -0800

    rhel 5 based distros can't do https through a proxy, so put some code in to handle that

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,18 +1,24 @@
 default[:cloudpassage][:agent_key] = ""
 default[:cloudpassage][:tag] = ""
-default[:cloudpassage][:deb_repo_uri] = "https://packages.cloudpassage.com/debian"
-default[:cloudpassage][:deb_repo_distribution] = "debian"
-default[:cloudpassage][:deb_repo_components] = ["main"]
-default[:cloudpassage][:rpm_repo_url] = "https://packages.cloudpassage.com/redhat/$basearch"
-default[:cloudpassage][:deb_key_location] = "https://packages.cloudpassage.com/cloudpassage.packages.key"
-default[:cloudpassage][:rpm_key_location] = "https://packages.cloudpassage.com/cloudpassage.packages.key"
-default[:cloudpassage][:win_installer_location] = "https://packages.cloudpassage.com/windows/cphalo-3.2.10-win64.exe"
-default[:cloudpassage][:grid] = "https://grid.cloudpassage.com/grid" 
 # This proxy url needs to be in the format of "hostname:port".  No http/https, etc. For example:
 # default[:cloudpassage][:proxy_url] = "proxyhost.cloudpassage.com:3128"
 default[:cloudpassage][:proxy_url] = ""
 default[:cloudpassage][:proxy_user] = "" 
 default[:cloudpassage][:proxy_pass] = ""
+default[:cloudpassage][:deb_repo_uri] = "https://packages.cloudpassage.com/debian"
+default[:cloudpassage][:deb_repo_distribution] = "debian"
+default[:cloudpassage][:deb_repo_components] = ["main"]
+default[:cloudpassage][:deb_key_location] = "https://packages.cloudpassage.com/cloudpassage.packages.key"
+# rhel 5 based distros can't do https through a proxy
+if (node.platform_version.to_i < 6) && (default[:cloudpassage][:proxy_url] != "")
+	default[:cloudpassage][:rpm_repo_url] = "http://packages.cloudpassage.com/redhat/$basearch"
+	default[:cloudpassage][:rpm_key_location] = "http://packages.cloudpassage.com/cloudpassage.packages.key"
+else
+	default[:cloudpassage][:rpm_repo_url] = "https://packages.cloudpassage.com/redhat/$basearch"
+	default[:cloudpassage][:rpm_key_location] = "https://packages.cloudpassage.com/cloudpassage.packages.key"
+end
+default[:cloudpassage][:win_installer_location] = "https://packages.cloudpassage.com/windows/cphalo-3.2.10-win64.exe"
+default[:cloudpassage][:grid] = "https://grid.cloudpassage.com/grid" 
 default[:cloudpassage][:readonly] = 'false'
 default[:cloudpassage][:usedns] = 'true'
 default[:cloudpassage][:label] = ''
